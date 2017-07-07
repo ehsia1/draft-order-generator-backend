ce4088078712bb43ddd9ee716e39e5b6855f1d9d
Evan Hsia <ehsia@Evans-MacBook-Pro.local>
2017-07-07 02:00:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.get('/fantasy-football-draft-order', function(req, res) {
+  if (!req.query) {
+    res.status(400).send('need query string');
+  } else if (!req.query.names || !req.query.names.length) {
+    res.status(400).send('need players');
+  } else {
+    var array = req.query.names;
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    res.status(200).send(array);
+  }
+});
+
+
+const port = 6095;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+module.exports = app;
