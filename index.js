b6cddf67ddba2c8dc644604e4a897ae7a37d3c1c
Evan Hsia <ehsia@Evans-MacBook-Pro.local>
2017-07-07 20:59:40 -0700

    random divisions

--- index.js
+++ index.js
@@ -34,6 +34,33 @@ app.get('/fantasy-football-draft-order', function(req, res) {
   }
 });
 
+app.get('/divisions-completely-random', function(req, res) {
+  if (!req.query) {
+    res.status(400).send('need query string');
+  } else if (!req.query.names || !req.query.names.length) {
+    res.status(400).send('need players');
+  } else {
+    var array = req.query.names;
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    var half = array.length / 2;
+    var east = array.splice(0, half);
+    var west = array;
+    var object = {east: east, west: west};
+    res.status(200).send(object);
+  }
+})
+
 
 const port = 6095;
 app.listen(port, function () {
